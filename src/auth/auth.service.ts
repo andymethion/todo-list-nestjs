3aedc165a7c6651b02ae7320d3fac56de27a8c9e
Andy Méthion <andymethion@gmail.com>
2026-03-26 15:57:31 +0100

    feat(auth, users): integrate TypeORM for User entity in Auth and Users modules

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/users/entities/user.entity';
+import { Repository } from 'typeorm';
 import { LoginDto } from './dto/login.dto';
 import { RegisterDto } from './dto/register.dto';
 
 @Injectable()
 export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
   login(loginDto: LoginDto) {
     return loginDto;
   }
